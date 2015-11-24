5eb1a5e5d059941f24e8ec486abc7108f54e102d
Kerem Gunes <qeremy@gmail.com>
2015-11-24 23:42:06 +0200

    Add util pack (first).

--- src/couch/util/util.go
+++ src/couch/util/util.go
@@ -1,1 +1,153 @@
 package util
+
+import (
+    _fmt "fmt"
+    _str "strings"
+    _strc "strconv"
+    _json "encoding/json"
+    _rex "regexp"
+)
+
+type Nil struct {}
+
+func Foo(a interface{}) {
+
+}
+
+func Type(args ...interface{}) string {
+    return _str.Trim(TypeReal(args[0]), " *<>{}[]")
+}
+func TypeReal(args ...interface{}) string {
+    return _fmt.Sprintf("%T", args[0])
+}
+
+func ToString(args ...interface{}) string {
+    return _fmt.Sprintf("%v", args[0]);
+}
+func String(input interface{}) string {
+    return _fmt.Sprintf("%v", input);
+}
+func Int(input interface{}) int {
+    result, err := _strc.Atoi(input.(string))
+    if err != nil {
+        return int(result)
+    }
+    return 0
+}
+func Number(input interface{}, inputType string) interface{} {
+    result, err := _strc.Atoi(input.(string))
+    if err != nil {
+        return nil
+    }
+    switch inputType {
+        // signed
+        case    "int": return int(result)
+        case   "int8": return int8(result)
+        case  "int16": return int16(result)
+        case  "int32": return int32(result)
+        case  "int64": return int64(result)
+        // unsigned
+        case   "uint": return uint(result)
+        case  "uint8": return uint8(result)
+        case "uint16": return uint16(result)
+        case "uint32": return uint32(result)
+        case "uint64": return uint64(result)
+    }
+    return 0
+}
+
+
+func IsEmpty(input interface{}) bool {
+    if input == nil || input == "" || input == 0 {
+        return true
+    }
+    return false
+}
+func IsEmptySet(input interface{}, inputDefault interface{}/*, inputType string*/) interface{} {
+    if IsEmpty(input) {
+        input = inputDefault
+        // switch inputType {
+        //     case "string":
+        //         input = ToString(inputDefault)
+        //     default:
+        //         panic("Unimplemeted type '"+ inputType +"' given!")
+        // }
+    }
+    return input
+}
+
+func Dump(args ...interface{}) {
+    _fmt.Println(args...)
+}
+func Dumps(args ...interface{}) {
+    var format string
+    for _, arg := range args {
+        _ = arg // silence..
+        format += "%+v "
+    }
+    _fmt.Printf("%s\n", _fmt.Sprintf(format, args...))
+}
+func Dumpf(format string, args ...interface{}) {
+    if format == "" {
+        for _, arg := range args {
+            _ = arg // silence..
+            format += "%+v "
+        }
+    }
+    _fmt.Printf("%s\n", _fmt.Sprintf(format, args...))
+}
+
+func Quote(input string) {
+    input = _str.Replace(input, "\"", "%22", -1)
+}
+
+// parsers
+func ParseUrl(url string) map[string]string {
+    var result = make(map[string]string)
+    var pattern = "(?P<Scheme>https?)://(?P<Host>[^:/]+)"   +
+                  "(?:\\:(?P<Port>\\d+))?(?P<Path>/[^?]+)?" +
+                  "(?:\\?(?P<Query>[^#]+))?"                +
+                  "(?:\\#(?P<Fragment>.*))?"
+    re, _ := _rex.Compile(pattern)
+    if re == nil {
+        return result
+    }
+    var match = re.FindStringSubmatch(url)
+    for i, name := range re.SubexpNames() {
+        if i != 0 {
+            result[name] = match[i]
+        }
+    }
+    if result["Path"] == "" {
+        result["Path"] = "/"
+    }
+    return result
+}
+
+func ParseHeaders(headers string) map[string]string {
+    var result = make(map[string]string)
+    if tmps := _str.Split(headers, "\r\n"); tmps != nil {
+        for _, tmp := range tmps {
+            var tmp = _str.SplitN(tmp, ":", 2)
+            // request | response check?
+            if len(tmp) == 1 {
+                // status line >> HTTP/1.0 200 OK
+                result["0"] = tmp[0]
+                continue
+            }
+            var key, value =
+                _str.TrimSpace(tmp[0]),
+                _str.TrimSpace(tmp[1]);
+            result[key] = value
+        }
+    }
+    return result
+}
+
+func ParseBody(in string, out interface{}) interface{} {
+    err := _json.Unmarshal([]byte(in), &out)
+    if err != nil {
+        panic(err)
+    }
+    return out
+}
