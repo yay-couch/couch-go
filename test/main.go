ea31629f4751cde7ea8f8c471bf5066b251bce0a
Kerem Gunes <qeremy@gmail.com>
2015-11-25 23:53:09 +0200

    test n test..

--- test/main.go
+++ test/main.go
@@ -16,25 +16,25 @@ func main() {
     response := client.DoRequest("GET /", nil, "", nil)
     // _dumpf("response >> %+v", response)
 
-    var responseBody = response.GetBody()
-    _dumpf("response: len(%d) body(%+v)", len(responseBody), responseBody)
+    // var responseBody = response.GetBody()
+    // _dumpf("response: len(%d) body(%+v)", len(responseBody), responseBody)
 
-    // type Response struct {
-    //     CouchDB string
-    //     Uuid    string
-    //     Version string
-    //     Vendor  map[string]string
-    // }
-    // body, err := u.ParseBody(response.GetBody(), &Response{})
-    // if err != nil {
-    //     _dumps(err)
-    //     return
-    // }
-    // _dumps(body)
-    // _dumps(body.(*Response).CouchDB)
-    // _dumps(body.(*Response).Uuid)
-    // _dumps(body.(*Response).Version)
-    // _dumps(body.(*Response).Vendor)
-    // _dumps(body.(*Response).Vendor["name"])
-    // _dumps(body.(*Response).Vendor["version"])
+    type Response struct {
+        CouchDB string
+        Uuid    string
+        Version string
+        Vendor  map[string]string
+    }
+    body, err := u.ParseBody(response.GetBody(), &Response{})
+    if err != nil {
+        _dumps(err)
+        return
+    }
+    _dumps(body)
+    _dumps(body.(*Response).CouchDB)
+    _dumps(body.(*Response).Uuid)
+    _dumps(body.(*Response).Version)
+    _dumps(body.(*Response).Vendor)
+    _dumps(body.(*Response).Vendor["name"])
+    _dumps(body.(*Response).Vendor["version"])
 }
