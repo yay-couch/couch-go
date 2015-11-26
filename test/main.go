2ba8e4c5aa19bee01a82ee70f552e71e6de286ed
Kerem Gunes <qeremy@gmail.com>
2015-11-26 20:34:43 +0200

    run test_uuid.

--- test/main.go
+++ test/main.go
@@ -1,6 +1,6 @@
 package main
 
-// import test_uuid "./test_uuid"
+import test_uuid "./test_uuid"
 // import test_client "./test_client"
 
 import u "./../src/couch/util"
@@ -8,6 +8,6 @@ import u "./../src/couch/util"
 var _dump, _dumps, _dumpf = u.Dump, u.Dumps, u.Dumpf
 
 func main() {
+    test_uuid.TestAll()
     // test_client.TestAll()
-    // test_uuid.TestAll()
 }
