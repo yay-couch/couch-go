356697381dbf66953e9edbde3c2f33b10d5233bd
Kerem Gunes <qeremy@gmail.com>
2015-11-26 18:02:59 +0200

    remove uuid mock-ups.

--- test/main.go
+++ test/main.go
@@ -19,16 +19,6 @@ import u "./../src/couch/util"
 var _dump, _dumps, _dumpf = u.Dump, u.Dumps, u.Dumpf
 
 func main() {
-    u1 := _uuid.Generate(_uuid.HEX_8)
-    _dumpf("%s", u1)
-    _dumpf("%d", len(u1))
-
-    // u2 := _uuid.New("123")
-    u2 := _uuid.New(123)
-    _dump(u2.Value)
-    _dump(u2.GetValue())
-    _dump(u2.ToString())
-
     // couch    := _couch.New(nil)
     // client   := _couch.NewClient(couch, nil)
     // // client   := _couch.NewClient(couch, map[string]interface{}{
