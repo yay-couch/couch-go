94add4f638bd845800e9a62372a1c414cc19884a
Kerem Gunes <qeremy@gmail.com>
2015-11-26 23:54:10 +0200

    pkg.server works..

--- src/couch/server/server.go
+++ src/couch/server/server.go
@@ -1,12 +1,17 @@
 package server
 
-import (
-    // "fmt"
-)
+/**
+ * Links
+ * - http://blog.golang.org/json-and-go
+ * - http://golang.org/pkg/encoding/json/#example_Unmarshal
+ */
 
 import _client   "./../client"
 // import _response "./../http/response"
 
+import u "./../util"
+// @tmp
+var _dump, _dumps, _dumpf = u.Dump, u.Dumps, u.Dumpf
 
 type Server struct {
     Client *_client.Client
@@ -22,4 +27,27 @@ func New(client *_client.Client) *Server {
 
 func (this *Server) Ping() bool {
     return (200 == this.Client.Head("/", nil, nil).GetStatusCode())
+}
+
+// func (this *Server) Info() string {
+func (this *Server) Info() map[string]interface{} {
+    type __ struct {
+        CouchDB string
+        Uuid    string
+        Version string
+        Vendor  map[string]string
+    }
+    data, err := this.Client.Get("/", nil, nil).GetBody(&__{})
+    if err != nil {
+        return nil
+    }
+    var result = make(map[string]interface{});
+    result["couchdb"] = data.(*__).CouchDB
+    result["uuid"]    = data.(*__).Uuid
+    result["version"] = data.(*__).Version
+    result["vendor"]  = map[string]string{
+           "name": data.(*__).Vendor["name"],
+        "version": data.(*__).Vendor["version"],
+    }
+    return result
 }
--- test/main.go
+++ test/main.go
@@ -1,13 +1,34 @@
 package main
 
-import test_uuid "./test_uuid"
+// import test_uuid "./test_uuid"
 // import test_client "./test_client"
+
+import _couch  "./../src/couch"
 
 import u "./../src/couch/util"
 // @tmp
 var _dump, _dumps, _dumpf = u.Dump, u.Dumps, u.Dumpf
 
 func main() {
-    test_uuid.TestAll()
+    couch  := _couch.New(nil)
+    client := _couch.NewClient(couch, nil)
+    server := _couch.NewServer(client)
+    // _dumps(server.Client)
+
+    // _dump(server.Ping())
+
+    var info = server.Info()
+    _dumps(info)
+    // _dumps(info["couchdb"])
+    // _dumps(info["vendor"].(map[string]string)["name"])
+    // _dumps(info["vendor.name"])
+
+    // var value = u.Extract("couchdb", info)
+    // var value = u.Extract("vendor.name", info)
+    var value = u.Extract("vendor.name", info)
+    _dumps(value == "Ubuntu")
+
+
+    // test_uuid.TestAll()
     // test_client.TestAll()
 }
