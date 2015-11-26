49eca564a2d53ec48de27899f3dc41145b32f880
Kerem Gunes <qeremy@gmail.com>
2015-11-26 19:49:39 +0200

    finish testing client stuffs.

--- test/main.go
+++ test/main.go
@@ -1,11 +1,11 @@
 package main
 
-import "./test_client"
+// import test_client "./test_client"
 
 import u "./../src/couch/util"
 // @tmp
 var _dump, _dumps, _dumpf = u.Dump, u.Dumps, u.Dumpf
 
 func main() {
-    test.TestClient()
+    // test_client.TestAll()
 }
