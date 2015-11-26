ac9f8e67fc00e40d370bf01dad3e9644e4452a97
Kerem Gunes <qeremy@gmail.com>
2015-11-26 19:45:44 +0200

    complete test_client for now.

--- test/test_client/test_client.go
+++ test/test_client/test_client.go
@@ -20,57 +20,80 @@ func Shutup() {
 func _doRequest(uri string) *_response.Response {
     couch  := _couch.New(nil)
     client := _couch.NewClient(couch, nil)
-    return client.DoRequest(uri, nil, "", nil)
-}
-
-/**
- * Test client.
- */
-func TestClient() {
-    couch  := _couch.New(nil)
-    client := _couch.NewClient(couch, nil)
     // or
     // client := _couch.NewClient(couch, map[string]interface{}{
     //     "Host": "127.0.0.1",
     // })
-    var response = client.DoRequest("GET /", nil, "", nil)
-    _dumpf("response >> %+v", response)
+    return client.DoRequest(uri, nil, "", nil)
 }
 
-// func TestClientResponse() {
-//     var response = _doRequest("GET /")
-//     var responseBody = response.GetBody()
-//     _dumpf("response: len(%d) body(%+v)", len(responseBody), responseBody)
-// }
-// func TestClientResponseStatusCode() {
-//     var response = _doRequest("GET /")
-//     var responseBody = response.GetBody()
-//     _dumpf("response: len(%d) body(%+v)", len(responseBody), responseBody)
-// }
-// func TestClientResponseStatusText() {
-//     var response = _doRequest("GET /")
-//     var responseBody = response.GetBody()
-//     _dumpf("response: len(%d) body(%+v)", len(responseBody), responseBody)
-// }
+/**
+ * TestClientResponseStatus
+ */
+func TestClientResponseStatus() {
+    var response = _doRequest("GET /")
+    _dumpf("Response Status >> %s", response.GetStatus())
+}
 
-// func TestClientResponseBodyParse() {
-//     var response = _doRequest("GET /")
-//     type Response struct {
-//         CouchDB string
-//         Uuid    string
-//         Version string
-//         Vendor  map[string]string
-//     }
-//     body, err := u.ParseBody(response.GetBody(), &Response{})
-//     if err != nil {
-//         _dumps(err)
-//         return
-//     }
-//     _dumps(body)
-//     _dumps(body.(*Response).CouchDB)
-//     _dumps(body.(*Response).Uuid)
-//     _dumps(body.(*Response).Version)
-//     _dumps(body.(*Response).Vendor)
-//     _dumps(body.(*Response).Vendor["name"])
-//     _dumps(body.(*Response).Vendor["version"])
-// }
+/**
+ * TestClientResponseStatusCode
+ */
+func TestClientResponseStatusCode() {
+    var response = _doRequest("GET /")
+    _dumpf("Response Status Code >> %d", response.GetStatusCode())
+}
+
+/**
+ * TestClientResponseStatusText
+ */
+func TestClientResponseStatusText() {
+    var response = _doRequest("GET /")
+    _dumpf("Response Status Text >> %s", response.GetStatusText())
+}
+
+/**
+ * TestClientResponseHeaders.
+ */
+func TestClientResponseHeaders(key string) {
+    var response = _doRequest("GET /")
+    if key == "" {
+        _dumpf("Response Headers >> %+v", response.GetHeaderAll())
+    } else {
+        _dumpf("Response Headers >> %s: %+v", key, response.GetHeader(key))
+    }
+}
+
+/**
+ * TestClientResponseBody
+ */
+func TestClientResponseBody() {
+    var response = _doRequest("GET /")
+    _dumpf("Response Body >> len: %d body: %+v",
+        len(response.GetBody()), response.GetBody())
+}
+
+/**
+ * TestClientResponseBodyParse.
+ */
+func TestClientResponseBodyParse() {
+    type Response struct {
+        CouchDB string
+        Uuid    string
+        Version string
+        Vendor  map[string]string
+    }
+
+    var response = _doRequest("GET /")
+    body, err := u.ParseBody(response.GetBody(), &Response{})
+    if err != nil {
+        _dumps(err)
+        return
+    }
+    _dumpf("Response Body Parsed >> type: %T value: %+v", body, body)
+    _dumpf("Repsonse Body Parsed >> couchdb: %s", body.(*Response).CouchDB)
+    _dumpf("Repsonse Body Parsed >> uuid: %s", body.(*Response).Uuid)
+    _dumpf("Repsonse Body Parsed >> version: %s", body.(*Response).Version)
+    _dumpf("Repsonse Body Parsed >> vendor: %s", body.(*Response).Vendor)
+    _dumpf("Repsonse Body Parsed >> vendor.name: %s", body.(*Response).Vendor["name"])
+    _dumpf("Repsonse Body Parsed >> vendor.version: %s", body.(*Response).Vendor["version"])
+}
