29ade173f15771bc07272d7bff431363e503d108
Kerem Gunes <qeremy@gmail.com>
2015-11-26 21:24:21 +0200

    start Server pkg.

--- src/couch/server/server.go
+++ src/couch/server/server.go
@@ -1,3 +1,23 @@
 package server
 
+import (
+    // "fmt"
+)
+
+import _client   "./../client"
+// import _response "./../http/response"
+
+
+type Server struct {
+    Client *_client.Client
+}
+
+func Shutup() {}
+
+func New(client *_client.Client) *Server {
+    return &Server{
+        Client: client,
+    }
+}
+
 
