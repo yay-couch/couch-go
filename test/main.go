fe1b4feddc026feaccde3e8db0b9055691f87bdd
Kerem Gunes <qeremy@gmail.com>
2015-11-26 18:54:17 +0200

    seperate tests..

--- test/main.go
+++ test/main.go
@@ -1,51 +1,11 @@
 package main
 
-// import _couch "./../src/couch"
-
-// import "encoding/hex"
-import (
-    // "fmt"
-    // "time"
-    // "math"
-    // "math/rand"
-    // "crypto/rand"
-    // "strconv"
-)
-
-import _uuid "./../src/couch/uuid"
+import "./test_client"
 
 import u "./../src/couch/util"
 // @tmp
 var _dump, _dumps, _dumpf = u.Dump, u.Dumps, u.Dumpf
 
 func main() {
-    // couch    := _couch.New(nil)
-    // client   := _couch.NewClient(couch, nil)
-    // // client   := _couch.NewClient(couch, map[string]interface{}{
-    // //     "Host": "127.0.0.1",
-    // // })
-    // response := client.DoRequest("GET /", nil, "", nil)
-    // _dumpf("response >> %+v", response)
-
-    // var responseBody = response.GetBody()
-    // _dumpf("response: len(%d) body(%+v)", len(responseBody), responseBody)
-
-    // type Response struct {
-    //     CouchDB string
-    //     Uuid    string
-    //     Version string
-    //     Vendor  map[string]string
-    // }
-    // body, err := u.ParseBody(response.GetBody(), &Response{})
-    // if err != nil {
-    //     _dumps(err)
-    //     return
-    // }
-    // _dumps(body)
-    // _dumps(body.(*Response).CouchDB)
-    // _dumps(body.(*Response).Uuid)
-    // _dumps(body.(*Response).Version)
-    // _dumps(body.(*Response).Vendor)
-    // _dumps(body.(*Response).Vendor["name"])
-    // _dumps(body.(*Response).Vendor["version"])
+    test.TestClient()
 }
