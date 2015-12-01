d23d8288b5fd6f37bdb273629a527f2f89e75c11
Kerem Gunes <qeremy@gmail.com>
2015-12-01 05:16:58 +0200

    add Post().

--- src/couch/client/client.go
+++ src/couch/client/client.go
@@ -117,7 +117,10 @@ func (this *Client) DoRequest(uri string, uriParams interface{},
 func (this *Client) Head(uri string, uriParams interface{}, headers interface{}) *_response.Response {
     return this.DoRequest(_request.METHOD_HEAD +" /"+ uri, uriParams, nil, headers)
 }
-
 func (this *Client) Get(uri string, uriParams interface{}, headers interface{}) *_response.Response {
     return this.DoRequest(_request.METHOD_GET +" /"+ uri, uriParams, nil, headers)
+}
+func (this *Client) Post(uri string, uriParams interface{}, body interface{},
+    headers interface{}) *_response.Response {
+    return this.DoRequest(_request.METHOD_POST +" /"+ uri, uriParams, body, headers)
 }
