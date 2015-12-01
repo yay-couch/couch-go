f6b5a694b678d46b5bc52f114e2c39c19fffd827
Kerem Gunes <qeremy@gmail.com>
2015-12-01 21:22:43 +0200

    add tmp stuff.

--- src/couch/database/database.go
+++ src/couch/database/database.go
@@ -1,1 +1,45 @@
 package database
+
+import u "./../util"
+// @tmp
+var _dump, _dumps, _dumpf = u.Dump, u.Dumps, u.Dumpf
+
+type Database struct {
+    Client *_client.Client
+    Name   string
+}
+
+func Shutup() {
+    u.Shutup()
+}
+
+func New(client *_client.Client, name string) *Database {
+    return &Database{
+        Client: client,
+    }
+}
+
+func (this *Server) Ping() bool {
+    // return (200 == this.Client.Head("/", nil, nil).GetStatusCode())
+}
+
+func (this *Server) Info() (map[string]interface{}, error) {
+    // type Data map[string]interface{}
+    // data, err := this.Client.Get("/", nil, nil).GetBodyData(&Data{})
+    // if err != nil {
+    //     return nil, err
+    // }
+    // var _return = make(map[string]interface{})
+    // for key, value := range *data.(*Data) {
+    //     switch value := value.(type) {
+    //         case map[string]interface{}:
+    //             _return[key] = make(map[string]string)
+    //             for kkey, vvalue := range value {
+    //                 _return[key].(map[string]string)[kkey] = vvalue.(string)
+    //             }
+    //         default:
+    //             _return[key] = value
+    //     }
+    // }
+    // return _return, nil
+}
