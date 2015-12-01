82a017c210fab375e1e53a2323d73bfff54edf2e
Kerem Gunes <qeremy@gmail.com>
2015-12-01 23:43:52 +0200

    add Replicate().

--- src/couch/database/database.go
+++ src/couch/database/database.go
@@ -46,3 +46,35 @@ func (this *Database) Create() bool {
 func (this *Database) Remove() bool {
     return (200 == this.Client.Delete(this.Name, nil, nil).GetStatusCode())
 }
+
+func (this *Database) Replicate(target string, targetCreate bool) (map[string]interface{}, error) {
+    var body = map[string]interface{}{
+               "source": this.Name,
+               "target": target,
+        "create_target": targetCreate,
+    }
+
+    type Data map[string]interface{}
+    data, err := this.Client.Post("/_replicate", nil, body, nil).GetBodyData(&Data{})
+    if err != nil {
+        return nil, err
+    }
+    var _return = make(map[string]interface{})
+    for key, value := range *data.(*Data) {
+        if key == "history" {
+            _return[key] = make(map[int]map[string]interface{})
+            for i, history := range value.([]interface{}) {
+                _return[key] = make([]map[string]interface{}, len(value.([]interface{})))
+                for kkey, vvalue := range history.(map[string]interface{}) {
+                    if _return[key].([]map[string]interface{})[i] == nil {
+                        _return[key].([]map[string]interface{})[i] = make(map[string]interface{})
+                    }
+                    _return[key].([]map[string]interface{})[i][kkey] = vvalue
+                }
+            }
+            continue
+        }
+        _return[key] = value
+    }
+    return _return, nil
+}
