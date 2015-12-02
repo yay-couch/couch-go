9e2c18f8f09bd4c74ba45b0c2d505918bdb65d07
Kerem Gunes <qeremy@gmail.com>
2015-12-02 05:48:32 +0200

    fix typo.

--- src/couch/http/request/request.go
+++ src/couch/http/request/request.go
@@ -119,7 +119,7 @@ func (this *Request) Send() string {
         if read, _ := reader.Read(buffer); read == 0 {
             break // eof
         }
-        response = _str.Trim(string(buffer), "\x00")
+        response += _str.Trim(string(buffer), "\x00")
     }
 
     // @debug
