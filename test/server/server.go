096221174aef4af5fad19c7a0ebb11d69cb7ec94
Kerem Gunes <qeremy@gmail.com>
2015-12-07 16:17:05 +0200

    remove tmp stuff.

--- test/server/server.go
+++ test/server/server.go
@@ -1,21 +1,24 @@
 package server
 
-import _couch  "./../../src/couch"
-import _server "./../../src/couch/server"
-
-import u "./../../src/couch/util"
-// @tmp
-var _dump, _dumps, _dumpf = u.Dump, u.Dumps, u.Dumpf
+import (
+    "./../../src/couch"
+    "./../../src/couch/util"
+)
 
 var (
     DEBUG = true
 )
 
-func _newServer() *_server.Server {
-    couch  := _couch.New(nil, DEBUG)
-    client := _couch.NewClient(couch, nil)
-    server := _couch.NewServer(client)
-    return server
+var (
+    Couch  *couch.Couch
+    Client *couch.Client
+    Server *couch.Server
+)
+
+func init() {
+    Couch  = couch.New(nil, DEBUG)
+    Client = couch.NewClient(Couch)
+    Server = couch.NewServer(Client);
 }
 
 /**
@@ -27,53 +30,53 @@ func TestAll() {}
  * TestPing
  */
 func TestPing() {
-    _dumpf("Server Ping >> %v ", _newServer().Ping())
+    util.Dumpf("Server Ping >> %v ", Server.Ping())
 }
 
 /**
  * TestInfo
  */
 func TestInfo() {
-    data, err := _newServer().Info()
+    data, err := Server.Info()
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Info >> %+v", data)
-    _dumpf("Server Info >> couchdb: %s", data["couchdb"])
+    util.Dumpf("Server Info >> %+v", data)
+    util.Dumpf("Server Info >> couchdb: %s", data["couchdb"])
     // or
-    _dumpf("Server Info >> couchdb: %s", u.Dig("couchdb", data))
-    _dumpf("Server Info >> uuid: %s", u.Dig("uuid", data))
-    _dumpf("Server Info >> version: %s", u.Dig("version", data))
+    util.Dumpf("Server Info >> couchdb: %s", util.Dig("couchdb", data))
+    util.Dumpf("Server Info >> uuid: %s", util.Dig("uuid", data))
+    util.Dumpf("Server Info >> version: %s", util.Dig("version", data))
     // or
-    _dumpf("Server Info >> vendor.name: %s", u.Dig("vendor.name", data))
-    _dumpf("Server Info >> vendor.version: %s", u.Dig("vendor.version", data))
-    _dumpf("Server Info >> vendor.name: %s", data["vendor"].(map[string]string)["name"])
-    _dumpf("Server Info >> vendor.version: %s", data["vendor"].(map[string]string)["version"])
+    util.Dumpf("Server Info >> vendor.name: %s", util.Dig("vendor.name", data))
+    util.Dumpf("Server Info >> vendor.version: %s", util.Dig("vendor.version", data))
+    util.Dumpf("Server Info >> vendor.name: %s", data["vendor"].(map[string]string)["name"])
+    util.Dumpf("Server Info >> vendor.version: %s", data["vendor"].(map[string]string)["version"])
 }
 
 /**
  * TestVersion
  */
 func TestVersion() {
-    data, err := _newServer().Version()
+    data, err := Server.Version()
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Version >> %s", data)
+    util.Dumpf("Server Version >> %s", data)
 }
 
 /**
  * TestGetActiveTasks
  */
 func TestGetActiveTasks() {
-    data, err := _newServer().GetActiveTasks()
+    data, err := Server.GetActiveTasks()
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Active Tasks >> %+v", data)
+    util.Dumpf("Server Active Tasks >> %+v", data)
     for _, task := range data {
-        _dumpf("Server Active Tasks >> 0.pid: %s", task["pid"])
-        _dumpf("Server Active Tasks >> 0.database: %s", task["database"])
+        util.Dumpf("Server Active Tasks >> 0.pid: %s", task["pid"])
+        util.Dumpf("Server Active Tasks >> 0.database: %s", task["database"])
     }
 }
 
@@ -81,74 +84,74 @@ func TestGetActiveTasks() {
  * TestGetAllDatabases
  */
 func TestGetAllDatabases() {
-    data, err := _newServer().GetAllDatabases()
+    data, err := Server.GetAllDatabases()
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Databases >> %+v", data)
-    _dumpf("Server Databases >> first: %s", data[0])
+    util.Dumpf("Server Databases >> %+v", data)
+    util.Dumpf("Server Databases >> first: %s", data[0])
 }
 
 /**
  * TestGetDatabaseUpdates
  */
 func TestGetDatabaseUpdates() {
-    data, err := _newServer().GetDatabaseUpdates(nil)
+    data, err := Server.GetDatabaseUpdates(nil)
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Updates >> %+v", data)
-    _dumpf("Server Updates >> db_name: %s", data["db_name"])
-    _dumpf("Server Updates >> type: %s", data["type"])
-    _dumpf("Server Updates >> ok: %v", data["ok"])
+    util.Dumpf("Server Updates >> %+v", data)
+    util.Dumpf("Server Updates >> db_name: %s", data["db_name"])
+    util.Dumpf("Server Updates >> type: %s", data["type"])
+    util.Dumpf("Server Updates >> ok: %v", data["ok"])
 }
 
 /**
  * TestGetLogs
  */
 func TestGetLogs() {
-    var data = _newServer().GetLogs(nil)
-    _dumps(data)
+    var data = Server.GetLogs(nil)
+    util.Dumps(data)
 }
 
 /**
  * TestGetStats
  */
 func TestGetStats() {
-    data, err := _newServer().GetStats("")
+    data, err := Server.GetStats("")
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Stats >> %+v", data)
-    _dumpf("Server Stats >> couchdb: %+v", data["couchdb"])
-    _dumpf("Server Stats >> couchdb.request_time: %+v", data["couchdb"]["request_time"])
-    _dumpf("Server Stats >> couchdb.request_time.description: %s", data["couchdb"]["request_time"]["description"])
-    _dumpf("Server Stats >> couchdb.request_time.description: %f", data["couchdb"]["request_time"]["current"])
-    _dumpf("Server Stats >> httpd_request_methods.GET.max: %v", data["httpd_request_methods"]["GET"]["max"])
+    util.Dumpf("Server Stats >> %+v", data)
+    util.Dumpf("Server Stats >> couchdb: %+v", data["couchdb"])
+    util.Dumpf("Server Stats >> couchdb.request_time: %+v", data["couchdb"]["request_time"])
+    util.Dumpf("Server Stats >> couchdb.request_time.description: %s", data["couchdb"]["request_time"]["description"])
+    util.Dumpf("Server Stats >> couchdb.request_time.description: %f", data["couchdb"]["request_time"]["current"])
+    util.Dumpf("Server Stats >> httpd_request_methods.GET.max: %v", data["httpd_request_methods"]["GET"]["max"])
 }
 
 /**
  * TestGetUuid
  */
 func TestGetUuid() {
-    data, err := _newServer().GetUuid()
+    data, err := Server.GetUuid()
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Uuid >> %s", data)
+    util.Dumpf("Server Uuid >> %s", data)
 }
 
 /**
  * TestGetUuids
  */
 func TestGetUuids() {
-    data, err := _newServer().GetUuids(3)
+    data, err := Server.GetUuids(3)
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Uuids >> %+v", data)
+    util.Dumpf("Server Uuids >> %+v", data)
     for i, _ := range data {
-        _dumpf("Server Uuids >> %d: %s", i, data[i])
+        util.Dumpf("Server Uuids >> %d: %s", i, data[i])
     }
 }
 
@@ -156,7 +159,7 @@ func TestGetUuids() {
  * TestReplicate
  */
 func TestReplicate() {
-    data, err := _newServer().Replicate(map[string]interface{}{
+    data, err := Server.Replicate(map[string]interface{}{
         "source": "foo",
         "target": "foo_replicate",
         "create_target": true,
@@ -164,63 +167,63 @@ func TestReplicate() {
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Database Replicate >> %+v", data)
-    _dumpf("Server Database Replicate >> ok: %v", data["ok"])
-    _dumpf("Server Database Replicate >> history.0: %v", u.Dig("0", data["history"]))
-    _dumpf("Server Database Replicate >> history.0.start_time: %s", u.Dig("0.start_time", data["history"]))
+    util.Dumpf("Server Database Replicate >> %+v", data)
+    util.Dumpf("Server Database Replicate >> ok: %v", data["ok"])
+    util.Dumpf("Server Database Replicate >> history.0: %v", util.Dig("0", data["history"]))
+    util.Dumpf("Server Database Replicate >> history.0.start_time: %s", util.Dig("0.start_time", data["history"]))
 }
 
 /**
  * TestRestart
  */
 func TestRestart() {
-    _dumpf("Server Restart >> %v ", _newServer().Restart())
+    util.Dumpf("Server Restart >> %v ", Server.Restart())
 }
 
 /**
  * TestGetConfig
  */
 func TestGetConfig() {
-    // data1, err := _newServer().GetConfig()
+    // data1, err := Server.GetConfig()
     // if err != nil {
     //     panic(err)
     // }
-    // _dumpf("Server Get Config >> %+v", data1)
-    // _dumpf("Server Get Config >> couchdb: %v", data1["couchdb"])
-    // _dumpf("Server Get Config >> couchdb.uuid: %s", data1["couchdb"]["uuid"])
+    // util.Dumpf("Server Get Config >> %+v", data1)
+    // util.Dumpf("Server Get Config >> couchdb: %v", data1["couchdb"])
+    // util.Dumpf("Server Get Config >> couchdb.uuid: %s", data1["couchdb"]["uuid"])
     // // or
-    // data2, err := _newServer().GetConfigSection("couchdb")
+    // data2, err := Server.GetConfigSection("couchdb")
     // if err != nil {
     //     panic(err)
     // }
-    // _dumpf("Server Get Config Section >> couchdb: %v", data2)
-    // _dumpf("Server Get Config Section >> couchdb.uuid: %s", data2["uuid"])
+    // util.Dumpf("Server Get Config Section >> couchdb: %v", data2)
+    // util.Dumpf("Server Get Config Section >> couchdb.uuid: %s", data2["uuid"])
     // or
-    data3, err := _newServer().GetConfigSectionKey("couchdb", "uuid")
+    data3, err := Server.GetConfigSectionKey("couchdb", "uuid")
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Get Config Section Key >> couchdb.uuid: %s", data3)
+    util.Dumpf("Server Get Config Section Key >> couchdb.uuid: %s", data3)
 }
 
 /**
  * TestSetConfig
  */
 func TestSetConfig() {
-    data, err := _newServer().SetConfig("couch", "foo", "The foo!")
+    data, err := Server.SetConfig("couch", "foo", "The foo!")
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Set Config >> %s", data)
+    util.Dumpf("Server Set Config >> %s", data)
 }
 
 /**
  * TestRemoveConfig
  */
 func TestRemoveConfig() {
-    data, err := _newServer().RemoveConfig("couch", "foo")
+    data, err := Server.RemoveConfig("couch", "foo")
     if err != nil {
         panic(err)
     }
-    _dumpf("Server Remove Config >> %s", data)
+    util.Dumpf("Server Remove Config >> %s", data)
 }
