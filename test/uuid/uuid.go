bc84f4b425a75b48c6ead7c9a66b047c25b01133
Kerem Gunes <qeremy@gmail.com>
2015-12-07 16:40:42 +0200

    reset tmp pkg naming.

--- test/uuid/uuid.go
+++ test/uuid/uuid.go
@@ -5,8 +5,6 @@ import (
     "./../../src/couch/uuid"
 )
 
-import _uuid "./../../src/couch/uuid"
-
 /**
  * TestAll
  */
@@ -24,47 +22,47 @@ func TestAll() {
  * TestDefault
  */
 func TestDefault() {
-    util.Dumpf("Uuid Default       >> %s", _uuid.GenerateDefault())
+    util.Dumpf("Uuid Default       >> %s", uuid.GenerateDefault())
 }
 
 /**
  * TestRfc
  */
 func TestRfc() {
-    util.Dumpf("Uuid RFC           >> %s", _uuid.GenerateRfc())
+    util.Dumpf("Uuid RFC           >> %s", uuid.GenerateRfc())
 }
 
 /**
  * TestHex8
  */
 func TestHex8() {
-    util.Dumpf("Uuid Hex 8         >> %s", _uuid.GenerateHex8())
+    util.Dumpf("Uuid Hex 8         >> %s", uuid.GenerateHex8())
 }
 
 /**
  * TestHex32
  */
 func TestHex32() {
-    util.Dumpf("Uuid Hex 32        >> %s", _uuid.GenerateHex32())
+    util.Dumpf("Uuid Hex 32        >> %s", uuid.GenerateHex32())
 }
 
 /**
  * TestHex40
  */
 func TestHex40() {
-    util.Dumpf("Uuid Hex 40        >> %s", _uuid.GenerateHex40())
+    util.Dumpf("Uuid Hex 40        >> %s", uuid.GenerateHex40())
 }
 
 /**
  * TestTimestamp
  */
 func TestTimestamp() {
-    util.Dumpf("Uuid Timestamp     >> %s", _uuid.GenerateTimestamp())
+    util.Dumpf("Uuid Timestamp     >> %s", uuid.GenerateTimestamp())
 }
 
 /**
  * TestTimestampNano
  */
 func TestTimestampNano() {
-    util.Dumpf("Uuid TimestampNano >> %s", _uuid.GenerateTimestampNano())
+    util.Dumpf("Uuid TimestampNano >> %s", uuid.GenerateTimestampNano())
 }
