d89964700e65e61d10c604186da795db3d48e8ac
Kerem Gunes <qeremy@gmail.com>
2015-12-07 16:44:39 +0200

    remove tmp stuff.

--- test/main.go
+++ test/main.go
@@ -1,46 +1,25 @@
 package main
 
 import (
-    // test_uuid "./uuid"
+    test_uuid "./uuid"
     // test_client "./client"
     // test_server "./server"
     // test_database "./database"
 )
 
-
-import (
-    "./../src/couch/util"
-)
-
-func init() {
-    util.Shutup()
-}
-
 func main() {
+    /* client */
+    // test_client.TestClientResponseStatus()
+    // ...
+
+    /* server */
+    // test_server.TestPing()
+    // ...
+
     /* database */
     // test_database.TestPing()
-    // test_database.TestInfo()
-    // test_database.TestCreate()
-    // test_database.TestRemove()
-    // test_database.TestReplicate()
-    // test_database.TestGetDocument()
-    // test_database.TestGetDocumentAll()
-    // test_database.TestCreateDocument()
-    // test_database.TestCreateDocumentAll()
-    // test_database.TestUpdateDocument()
-    // test_database.TestUpdateDocumentAll()
-    // test_database.TestDeleteDocument()
-    // test_database.TestDeleteDocumentAll()
-    // test_database.TestGetChanges()
-    // test_database.TestCompact()
-    // test_database.TestEnsureFullCommit()
-    // test_database.TestViewCleanup()
-    // test_database.TestViewTemp()
-    // test_database.TestGetSecurity()
-    // test_database.TestSetSecurity()
-    // test_database.TestPurge()
-    // test_database.TestGetMissingRevisions()
-    // test_database.TestGetMissingRevisionsDiff()
-    // test_database.TestGetRevisionLimit()
-    // test_database.TestSetRevisionLimit()
+    // ...
+
+    /* uuid */
+    test_uuid.TestAll()
 }
