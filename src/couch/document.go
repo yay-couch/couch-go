1085560a626b6d0f56a50022e87222d44d155107
Kerem Gunes <qeremy@gmail.com>
2015-12-15 19:45:11 +0200

    remove unneeded tmp stuff, modify NewDocument(), add SetId().

--- src/couch/document.go
+++ src/couch/document.go
@@ -1,8 +1,8 @@
 package couch
 
-import u "./../util"
-// @tmp
-var _dump, _dumps, _dumpf = u.Dump, u.Dumps, u.Dumpf
+import (
+    "./util"
+)
 
 type Document struct {
     Id          string
@@ -10,31 +10,19 @@ type Document struct {
     Deleted     bool
     Attachments []DocumentAttachment
     Data        map[string]interface{}
-    // Database    *_database.Database
+    Database    *Database
 }
 
-func Shutup() {}
-
-func NewDocument() *Document {
-    return &Document{
-        //
+func NewDocument(database *Database, data map[string]interface{}) *Document {
+    var this = &Document{
+        Database: database,
     }
+    if data != nil {
+        this.SetData(data)
+    }
+    return this
 }
 
-// func (this *Document) SetId(id interface{}) {
-//     this.Id = u.String(id)
-// }
-// func (this *Document) SetRev(rev string) {
-//     this.Rev = rev
-// }
-// func (this *Document) SetRev(deleted bool) {
-//     this.Deleted = deleted
-// }
-// func (this *Document) SetData(data map[string]interface{}) {
-//     if this.Data == nil {
-//         this.Data = make(map[string]interface{})
-//     }
-//     for key, value := range data.(map[string]interface{}) {
-//         this.Data[key] = value
-//     }
-// }
+func (this *Document) SetId(id interface{}) {
+    this.Id = id.(string)
+}
