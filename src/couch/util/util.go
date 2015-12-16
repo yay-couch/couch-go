4eb6697d4ce8de119072a79618efc6105aa4837b
Kerem Gunes <qeremy@gmail.com>
2015-12-17 01:49:18 +0200

    add _shiftInt().

--- src/couch/util/util.go
+++ src/couch/util/util.go
@@ -394,6 +394,11 @@ func MapSliceString(length interface{}) []string {
 }
 
 // local stuff
+func _shiftInt(slice *[]int) int {
+    var value = (*slice)[0]
+    *slice = (*slice)[1 : len(*slice)]
+    return value
+}
 func _shiftString(slice *[]string) string {
     var value = (*slice)[0]
     *slice = (*slice)[1 : len(*slice)]
