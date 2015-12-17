babec4a2737e1a639d498b21ebf3f4e5595730d2
Kerem Gunes <qeremy@gmail.com>
2015-12-17 23:28:50 +0200

    update document_attachment pkg.

--- src/couch/document_attachment.go
+++ src/couch/document_attachment.go
@@ -1,17 +1,31 @@
 package couch
 
+import (
+    _path "path"
+)
+
 type DocumentAttachment struct {
     Document    *Document
-    file        string
-    fileNam     string
-    data        string
-    dataLength  uint
-    contentType string
-    digest      string
+    File        string
+    FileName    string
+    Data        string
+    DataLength  uint
+    ContentType string
+    Digest      string
 }
 
-func NewDocumentAttachment() *DocumentAttachment {
-    return &DocumentAttachment{
-        //
+func NewDocumentAttachment(document *Document, file, fileName string) *DocumentAttachment {
+    var this = &DocumentAttachment{}
+    if document != nil {
+        this.Document = document
     }
+    if file != "" {
+        this.File = file
+        if fileName != "" {
+            this.FileName = fileName
+        } else {
+            this.FileName = _path.Base(fileName)
+        }
+    }
+    return this
 }
