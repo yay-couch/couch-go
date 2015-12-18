599ead08d175ed48b2809e5027b0219f4594fbf7
Kerem Gunes <qeremy@gmail.com>
2015-12-18 20:24:49 +0200

    add TestFind().

--- test/document_attachment/document_attachment.go
+++ test/document_attachment/document_attachment.go
@@ -41,3 +41,15 @@ func TestPing() {
     var docAttc = _documentAttachment("./attc.txt", "")
     util.Dumpf("Document Ping >> %v", docAttc.Ping(200))
 }
+
+/**
+ * TestFind
+ */
+func TestFind() {
+    var docAttc = _documentAttachment("./attc.txt", "").Find()
+    util.Dumpf("Document Find >> %v", docAttc)
+    util.Dumpf("Document Find >> content: %s", docAttc["content"])
+    util.Dumpf("Document Find >> content_type: %s", docAttc["content_type"])
+    util.Dumpf("Document Find >> content_length: %d", docAttc["content_length"])
+    util.Dumpf("Document Find >> digest: %s", docAttc["digest"])
+}
