23f3199e8671ab9f9f2bc9173cff26b0cd35e458
Kerem Gunes <qeremy@gmail.com>
2015-12-18 20:24:08 +0200

    add Find().

--- src/couch/document_attachment.go
+++ src/couch/document_attachment.go
@@ -71,3 +71,44 @@ func (this *DocumentAttachment) Ping(statusCodes ...uint16) bool {
     }
     return false
 }
+
+func (this *DocumentAttachment) Find() map[string]interface{} {
+    if this.Document == nil {
+        panic("Attachment document is not defined!")
+    }
+    var docId = this.Document.GetId()
+    var docRev = this.Document.GetRev()
+    if docId == "" {
+        panic("Attachment document _id is required!")
+    }
+    if this.FileName == "" {
+        panic("Attachment file name is required!")
+    }
+    var query = util.Param(nil)
+    if docRev != "" {
+        query["rev"] = docRev
+    }
+    var headers = util.Param(nil)
+    if this.Digest != "" {
+        headers["If-None-Match"] = util.Quote(this.Digest)
+    }
+    headers["Accept"] = "*/*"
+    headers["Content-Type"] = nil
+
+    var _return = util.Map()
+    var database = this.Document.GetDatabase()
+    var response = database.Client.Get(util.StringFormat("%s/%s/%s",
+        database.Name, docId, util.UrlEncode(this.FileName)), query, headers)
+    var statusCode = response.GetStatusCode()
+    if  statusCode == 200 || statusCode == 304 {
+        _return["content"] = response.GetBody()
+        _return["content_type"] = response.GetHeader("Content-Type")
+        _return["content_length"] = util.UInt(response.GetHeader("Content-Length"))
+        var md5 = response.GetHeader("Content-MD5")
+        if md5 == nil {
+            md5 = response.GetHeader("ETag")
+        }
+        _return["digest"] = "md5-"+ util.Trim(md5.(string), "\"")
+    }
+    return _return
+}
