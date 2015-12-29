8408cdef5f0bc36d369195d399059630d7adb1f0
Kerem Gunes <qeremy@gmail.com>
2015-12-29 18:26:01 +0200

    add license & docs & optimize.

--- src/couch/server.go
+++ src/couch/server.go
@@ -1,120 +1,190 @@
+// Copyright 2015 Kerem Güneş
+//    <http://qeremy.com>
+//
+// Apache License, Version 2.0
+//    <http://www.apache.org/licenses/LICENSE-2.0>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// @package couch
+// @uses    couch.util
+// @author  Kerem Güneş <qeremy[at]gmail[dot]com>
 package couch
 
 import (
     "couch/util"
 )
 
+// @object couch.Server
 type Server struct {
     Client *Client
 }
 
+// Constructor.
+//
+// @param  client *couch.Client
+// @return (*couch.Server)
 func NewServer(client *Client) *Server {
     return &Server{
         Client: client,
     }
 }
 
+// Ping.
+//
+// @return (bool)
 func (this *Server) Ping() bool {
     return (200 == this.Client.Head("/", nil, nil).GetStatusCode())
 }
 
+// Info.
+//
+// @return (map[string]interface{}, error)
 func (this *Server) Info() (map[string]interface{}, error) {
     data, err := this.Client.Get("/", nil, nil).GetBodyData(nil)
     if err != nil {
         return nil, err
     }
-    var _return = util.Map()
+
+    var ret = util.Map()
     for key, value := range data.(map[string]interface{}) {
         switch value := value.(type) {
             case map[string]interface{}:
-                _return[key] = util.MapString()
+                ret[key] = util.MapString()
                 for kkey, vvalue := range value {
-                    _return[key].(map[string]string)[kkey] = vvalue.(string)
+                    ret[key].(map[string]string)[kkey] = vvalue.(string)
                 }
             default:
-                _return[key] = value
+                ret[key] = value
         }
     }
-    return _return, nil
+
+    return ret, nil
 }
 
+// Version.
+//
+// @return (string, error)
 func (this *Server) Version() (string, error) {
     data, err := this.Info()
     if err != nil {
         return "", err
     }
+
     return data["version"].(string), nil
 }
 
+// Get active tasks.
+//
+// @return ([]map[string]interface{}, error)
 func (this *Server) GetActiveTasks() ([]map[string]interface{}, error) {
     data, err := this.Client.Get("/_active_tasks", nil, nil).GetBodyData(nil)
     if err != nil {
         return nil, err
     }
-    var _return = util.MapList(data)
+
+    var ret = util.MapList(data)
     for i, data := range data.([]interface{}) {
-        _return[i] = util.Map()
+        ret[i] = util.Map()
         for key, value := range data.(map[string]interface{}) {
-            _return[i][key] = value
+            ret[i][key] = value
         }
     }
-    return _return, nil
+
+    return ret, nil
 }
 
+// Get all databases.
+//
+// @return ([]string, error)
 func (this *Server) GetAllDatabases() ([]string, error) {
     data, err := this.Client.Get("/_all_dbs", nil, nil).GetBodyData(nil)
     if err != nil {
         return nil, err
     }
-    var _return = util.MapSliceString(data)
+
+    var ret = util.MapSliceString(data)
     for i, db := range data.([]interface{}) {
-        _return[i] = db.(string)
+        ret[i] = db.(string)
     }
-    return _return, nil
+
+    return ret, nil
 }
 
+// Get database updates.
+//
+// @param  query interface{}
+// @return (map[string]interface{}, error)
 func (this *Server) GetDatabaseUpdates(query interface{}) (map[string]interface{}, error) {
     data, err := this.Client.Get("/_db_updates", query, nil).GetBodyData(nil)
     if err != nil {
         return nil, err
     }
+
     return map[string]interface{}{
-        "db_name": data.(map[string]interface{})["db_name"],
-           "type": data.(map[string]interface{})["type"],
-             "ok": data.(map[string]interface{})["ok"],
+             "ok": util.DigBool("ok", data),
+           "type": util.DigString("type", data),
+        "db_name": util.DigString("db_name", data),
     }, nil
 }
 
+// Get logs.
+//
+// @param  query interface{}
+// @return (string)
 func (this *Server) GetLogs(query interface{}) string {
     return this.Client.Get("/_log", query, nil).GetBody()
 }
 
+// Get stats.
+//
+// @param  path string
+// @return (map[string]map[string]map[string]interface{}, error)
 func (this *Server) GetStats(path string) (map[string]map[string]map[string]interface{}, error) {
     data, err := this.Client.Get("/_stats/"+ path, nil, nil).GetBodyData(nil)
     if err != nil {
         return nil, err
     }
-    var _return = make(map[string]map[string]map[string]interface{})
+
+    var ret = make(map[string]map[string]map[string]interface{})
     for i, data := range data.(map[string]interface{}) {
-        _return[i] = make(map[string]map[string]interface{})
+        ret[i] = make(map[string]map[string]interface{})
         for ii, ddata := range data.(map[string]interface{}) {
-            _return[i][ii] = make(map[string]interface{})
+            ret[i][ii] = make(map[string]interface{})
             for key, value := range ddata.(map[string]interface{}) {
-                _return[i][ii][key] = value
+                ret[i][ii][key] = value
             }
         }
     }
-    return _return, nil
+
+    return ret, nil
 }
 
+// Get UUID.
+//
+// @return (string, error)
 func (this *Server) GetUuid() (string, error) {
     data, err := this.GetUuids(1)
     if err != nil {
         return "", err
     }
+
     return data[0], nil
 }
 
+// Get UUIDs.
+//
+// @return ([]string, error)
 func (this *Server) GetUuids(count int) ([]string, error) {
     var query = util.ParamList(
         "count", count,
@@ -123,92 +193,139 @@ func (this *Server) GetUuids(count int) ([]string, error) {
     if err != nil {
         return nil, err
     }
-    var _return = util.MapSliceString(count)
+
+    var ret = util.MapSliceString(count)
     for i, uuid := range data.(map[string]interface{})["uuids"].([]interface{}) {
-        _return[i] = uuid.(string)
+        ret[i] = uuid.(string)
     }
-    return _return, nil
+
+    return ret, nil
 }
 
+// Get UUID.
+//
+// @param  body map[string]interface{}
+// @return (map[string]interface{}, error)
+// @panics
 func (this *Server) Replicate(body map[string]interface{}) (map[string]interface{}, error) {
     body = util.Param(body)
     if body["source"] == nil || body["target"] == nil {
         panic("Both source & target required!")
     }
+
     data, err := this.Client.Post("/_replicate", nil, body, nil).GetBodyData(nil)
     if err != nil {
         return nil, err
     }
-    var _return = util.Map()
+
+    var ret = util.Map()
     for key, value := range data.(map[string]interface{}) {
+        // grap, set & pass history field
         if key == "history" {
-            _return["history"] = util.MapList(value)
+            ret["history"] = util.MapList(value)
             for i, history := range value.([]interface{}) {
-                _return["history"].([]map[string]interface{})[i] = util.Map()
+                ret["history"].([]map[string]interface{})[i] = util.Map()
                 for kkey, vvalue := range history.(map[string]interface{}) {
-                    _return["history"].([]map[string]interface{})[i][kkey] = vvalue
+                    ret["history"].([]map[string]interface{})[i][kkey] = vvalue
                 }
             }
             continue
         }
-        _return[key] = value
+        ret[key] = value
     }
-    return _return, nil
+
+    return ret, nil
 }
 
+// Restart.
+//
+// @return (bool)
 func (this *Server) Restart() bool {
     return (202 == this.Client.Post("/_restart", nil, nil, nil).GetStatusCode())
 }
 
+// Get config.
+//
+// @return (map[string]map[string]interface{}, error)
 func (this *Server) GetConfig() (map[string]map[string]interface{}, error) {
     data, err := this.Client.Get("/_config", nil, nil).GetBodyData(nil)
     if err != nil {
         return nil, err
     }
-    var _return = util.MapMapString()
+
+    var ret = util.MapMapString()
     for key, value := range data.(map[string]interface{}) {
-        _return[key] = util.Map()
+        ret[key] = util.Map()
         for kkey, vvalue := range value.(map[string]interface{}) {
-            _return[key][kkey] = vvalue
+            ret[key][kkey] = vvalue
         }
     }
-    return _return, nil
+
+    return ret, nil
 }
+
+// Get config section.
+//
+// @param  section string
+// @return (map[string]map[string]interface{}, error)
 func (this *Server) GetConfigSection(section string) (map[string]interface{}, error) {
     data, err := this.GetConfig()
     if err != nil {
         return nil, err
     }
+
     return data[section], nil
 }
+
+// Get config section value bey key.
+//
+// @param  section string
+// @param  key string
+// @return (string, error)
 func (this *Server) GetConfigSectionKey(section string, key string) (string, error) {
     data, err := this.GetConfig()
     if err != nil {
         return "", err
     }
+
     return data[section][key].(string), nil
 }
 
+// Set config.
+//
+// @param  section string
+// @param  key     string
+// @param  value   string
+// @return (string, error)
+// @panics
 func (this *Server) SetConfig(section, key, value string) (string, error) {
     if section == "" || key == "" {
         panic("Both section & key required!")
     }
-    data, err := this.Client.Put("/_config/"+ section +"/"+ key, nil, value, nil).
-        GetBodyData(nil)
+
+    data, err := this.Client.Put("/_config/"+ section +"/"+ key, nil, value, nil).GetBodyData(nil)
     if err != nil {
         return "", err
     }
+
     return data.(string), nil
 }
 
+// Remove config.
+//
+// @param  section string
+// @param  key     string
+// @return (string, error)
+// @panics
 func (this *Server) RemoveConfig(section, key string) (string, error) {
     if section == "" || key == "" {
         panic("Both section & key required!")
     }
-    data, err := this.Client.Delete("/_config/"+ section +"/"+ key, nil, nil).
-        GetBodyData(nil)
+
+    data, err := this.Client.Delete("/_config/"+ section +"/"+ key, nil, nil).GetBodyData(nil)
     if err != nil {
         return "", err
     }
+
     return data.(string), nil
 }
