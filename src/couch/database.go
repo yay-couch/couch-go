447b118af1b2c335be6d3b16677cec553b3a2dd7
Kerem Gunes <qeremy@gmail.com>
2015-12-29 05:21:20 +0200

    add/update docs.

--- src/couch/database.go
+++ src/couch/database.go
@@ -75,12 +75,12 @@ func (this *Database) Info() (map[string]interface{}, error) {
         return nil, err
     }
 
-    var r = util.Map()
+    var ret = util.Map()
     for key, value := range data.(map[string]interface{}) {
-        r[key] = value
+        ret[key] = value
     }
 
-    return r, nil
+    return ret, nil
 }
 
 // Create database.
@@ -153,23 +153,25 @@ func (this *Database) GetDocument(key string) (map[string]interface{}, error) {
         return nil, err
     }
 
-    var _return = util.Map()
+    var ret = util.Map()
+
     for _, doc := range data.(*DatabaseDocumentList).Rows {
-        _return["id"]    = doc.Id
-        _return["key"]   = doc.Key
-        _return["value"] = map[string]string{"rev": doc.Value["rev"].(string)}
-        _return["doc"]   = map[string]interface{}{}
+        ret["id"]    = doc.Id
+        ret["key"]   = doc.Key
+        ret["value"] = map[string]string{"rev": doc.Value["rev"].(string)}
+        ret["doc"]   = map[string]interface{}{}
         // fill doc field
         for key, value := range doc.Doc {
-            _return["doc"].(map[string]interface{})[key] = value
+            ret["doc"].(map[string]interface{})[key] = value
         }
     }
-    return _return, nil
+
+    return ret, nil
 }
 
 // Get documents.
 //
-// @param  query map[string]interface{}, error
+// @param  query map[string]interface{}
 // @param  keys  []string
 // @return map[string]interface{}, error
 func (this *Database) GetDocumentAll(
@@ -179,16 +181,20 @@ func (this *Database) GetDocumentAll(
         query["include_docs"] = true
     }
 
+    // short?
+    type ddl DatabaseDocumentList
+
     // make a reusable lambda
-    var fun = func(data interface{}, err error) (map[string]interface{}, error) {
+    var _func = func(data interface{}, err error) (map[string]interface{}, error) {
         if err != nil {
             return nil, err
         }
 
         var ret = util.Map()
-        var rows = data.(*DatabaseDocumentList).Rows
-        ret["offset"]     = data.(*DatabaseDocumentList).Offset
-        ret["total_rows"] = data.(*DatabaseDocumentList).TotalRows
+        ret["offset"]     = data.(*ddl).Offset
+        ret["total_rows"] = data.(*ddl).TotalRows
+
+        var rows = data.(*ddl).Rows
         ret["rows"]       = util.MapList(len(rows))
 
         // append docs
@@ -203,37 +209,47 @@ func (this *Database) GetDocumentAll(
 
         return ret, nil
     }
+
     if keys == nil {
-        return fun(
-            this.Client.Get(this.Name +"/_all_docs", query, nil).
-                GetBodyData(&DatabaseDocumentList{}))
+        // get all
+        return _func(
+            this.Client.Get(this.Name +"/_all_docs", query, nil).GetBodyData(&ddl{}))
     } else {
         var body = util.ParamList("keys", keys)
-        return fun(
-            this.Client.Post(this.Name +"/_all_docs", query, body, nil).
-                GetBodyData(&DatabaseDocumentList{}))
+        // get all only matched keys
+        return _func(
+            this.Client.Post(this.Name +"/_all_docs", query, body, nil).GetBodyData(&ddl{}))
     }
 }
 
-func (this *Database) CreateDocument(document interface{}) (
-        map[string]interface{}, error) {
+// Create document.
+//
+// @param  document map[string]interface{}
+// @return map[string]interface{}, error
+func (this *Database) CreateDocument(
+    document interface{}) (map[string]interface{}, error) {
     data, err := this.CreateDocumentAll([]interface{}{document})
     if err != nil {
         return nil, err
     }
+
     if data := data[0]; data != nil {
         return data, nil
     }
+
     return nil, nil
 }
 
-func (this *Database) CreateDocumentAll(documents []interface{}) (
-        []map[string]interface{}, error) {
+// Create documents.
+//
+// @param  document []map[string]interface{}
+// @return []map[string]interface{}, error
+func (this *Database) CreateDocumentAll(
+    documents []interface{}) ([]map[string]interface{}, error) {
     var docs = util.MapList(documents)
     for i, doc := range documents {
-        if docs[i] == nil {
-            docs[i] = util.Map()
-        }
+        if docs[i] == nil { docs[i] = util.Map() }
+        // filter documents
         for key, value := range doc.(map[string]interface{}) {
             // this is create method, no update allowed
             if key == "_id" || key == "_id" || key == "_deleted" {
@@ -242,38 +258,51 @@ func (this *Database) CreateDocumentAll(documents []interface{}) (
             docs[i][key] = value
         }
     }
+
     var body = util.ParamList("docs", docs)
-    data, err := this.Client.Post(this.Name +"/_bulk_docs", nil, body, nil).
-        GetBodyData(nil)
+    data, err := this.Client.Post(this.Name +"/_bulk_docs", nil, body, nil).GetBodyData(nil)
     if err != nil {
         return nil, err
     }
-    var _return = util.MapList(data)
+
+    var ret = util.MapList(data)
     for i, doc := range data.([]interface{}) {
-        if _return[i] == nil {
-            _return[i] = util.Map()
-        }
+        if ret[i] == nil { ret[i] = util.Map() }
+
         for key, value := range doc.(map[string]interface{}) {
-            _return[i][key] = value
+            ret[i][key] = value
         }
     }
-    return _return, nil
+
+    return ret, nil
 }
 
-func (this *Database) UpdateDocument(document interface{}) (
-        map[string]interface{}, error) {
+// Update document.
+//
+// @param  document map[string]interface{}
+// @return map[string]interface{}, error
+// @panics
+func (this *Database) UpdateDocument(
+    document interface{}) (map[string]interface{}, error) {
     data, err := this.UpdateDocumentAll([]interface{}{document})
     if err != nil {
         return nil, err
     }
+
     if data := data[0]; data != nil {
         return data, nil
     }
+
     return nil, nil
 }
 
-func (this *Database) UpdateDocumentAll(documents []interface{}) (
-        []map[string]interface{}, error) {
+// Update documents.
+//
+// @param  document []map[string]interface{}
+// @return []map[string]interface{}, error
+// @panics
+func (this *Database) UpdateDocumentAll(
+    documents []interface{}) ([]map[string]interface{}, error) {
     var docs = util.MapList(documents)
     for i, doc := range documents {
         if docs[i] == nil {
@@ -287,65 +316,85 @@ func (this *Database) UpdateDocumentAll(documents []interface{}) (
             panic("Both _id & _rev fields are required!")
         }
     }
+
     var body = util.ParamList("docs", docs)
-    data, err := this.Client.Post(this.Name +"/_bulk_docs", nil, body, nil).
-        GetBodyData(nil)
+    data, err := this.Client.Post(this.Name +"/_bulk_docs", nil, body, nil).GetBodyData(nil)
     if err != nil {
         return nil, err
     }
-    var _return = util.MapList(data)
+
+    var ret = util.MapList(data)
     for i, doc := range data.([]interface{}) {
-        if _return[i] == nil {
-            _return[i] = util.Map()
-        }
+        if ret[i] == nil { ret[i] = util.Map() }
+
         for key, value := range doc.(map[string]interface{}) {
-            _return[i][key] = value
+            ret[i][key] = value
         }
     }
-    return _return, nil
+
+    return ret, nil
 }
 
-func (this *Database) DeleteDocument(document interface{}) (
-        map[string]interface{}, error) {
+// Delete documents.
+//
+// @param  document map[string]interface{}
+// @return map[string]interface{}, error
+// @panics
+func (this *Database) DeleteDocument(
+    document interface{}) (map[string]interface{}, error) {
     data, err := this.DeleteDocumentAll([]interface{}{document})
     if err != nil {
         return nil, err
     }
+
     if data := data[0]; data != nil {
         return data, nil
     }
+
     return nil, nil
 }
 
+// Delete documents.
+//
+// @param  document []map[string]interface{}
+// @return []map[string]interface{}, error
+// @panics
 func (this *Database) DeleteDocumentAll(documents []interface{}) (
         []map[string]interface{}, error) {
     for i, _ := range documents {
         // just add "_deleted" param into document
         documents[i].(map[string]interface{})["_deleted"] = true
     }
+
     return this.UpdateDocumentAll(documents)
 }
 
-func (this *Database) GetChanges(query map[string]interface{}, docIds []string) (
-        map[string]interface{}, error) {
+// Get changes.
+//
+// @param  query map[string]interface{}
+// @return []map[string]interface{}, error
+func (this *Database) GetChanges(
+    query map[string]interface{}, docIds []string) (map[string]interface{}, error) {
     query = util.Param(query)
     if docIds != nil {
         query["filter"] = "_doc_ids"
     }
+
     var body = util.ParamList("doc_ids", docIds)
     data, err := this.Client.Post(this.Name +"/_changes", query, body, nil).
         GetBodyData(nil)
     if err != nil {
         return nil, err
     }
-    var _return = util.Map()
-    _return["last_seq"] = util.Dig("last_seq", data)
-    _return["results"]  = util.MapList(0) // set empty as default
+
+    var ret = util.Map()
+    ret["last_seq"] = util.Dig("last_seq", data)
+    ret["results"]  = util.MapList(0) // set empty as default
     if results := data.(map[string]interface{})["results"].([]interface{});
        results != nil {
-        _return["results"] = util.MapList(results) // @overwrite
+        ret["results"] = util.MapList(results) // @overwrite
         for i, result := range results {
-            _return["results"].([]map[string]interface{})[i] = map[string]interface{}{
+            ret["results"].([]map[string]interface{})[i] = map[string]interface{}{
                      "id": util.Dig("id", result),
                     "seq": util.Dig("seq", result),
                 "deleted": util.Dig("deleted", result),
@@ -353,15 +402,20 @@ func (this *Database) GetChanges(query map[string]interface{}, docIds []string) 
             }
         }
     }
-    return _return, nil
+
+    return ret, nil
 }
 
+// Compact.
+//
+// @param  ddoc string
+// @return []map[string]interface{}, error
 func (this *Database) Compact(ddoc string) (map[string]interface{}, error) {
-    data, err := this.Client.Post(this.Name +"/_compact/"+ ddoc, nil, nil, nil).
-        GetBodyData(nil)
+    data, err := this.Client.Post(this.Name +"/_compact/"+ ddoc, nil, nil, nil).GetBodyData(nil)
     if err != nil {
         return nil, err
     }
+
     return map[string]interface{}{
         "ok": util.DigBool("ok", data),
     }, nil
